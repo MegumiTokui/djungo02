e5f348e5491f35aa0a34e8b4f5bb7aa9a90f3deb
MegumiTokui <hojo.miku@gmail.com>
2019-02-27 15:27:13 +0530

    first commit

---
+++ newww/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("hi hghgfhfh")
+
+def first(request):
+    return render(request, 'newww/home.html')
+
+def about(request):
+    return  render(request, 'newww/about.html')
+
+def contact(request):
+    return  render(request, 'newww/contact.html')   
